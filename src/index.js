7a5df2fd67d3e4eb002a39794328b502a5df12c5
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-13 23:11:05 -0500

    added reducers in store:

--- src/index.js
+++ src/index.js
@@ -1,11 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+// admin 
+const feedback = (state = [], action) => {
+    if (action.type === "SAVE_FEEDBACK") {
+        return action.payload;
+    }
+}
+
+// feeling
+const feeling = (state = [], action) => {
+  if (action.type === "SAVE_FEELING") {
+    return action.payload;
+  }
+  return state;
+};
+
+// comprehension 
+const comprehension = (state = [], action) => {
+    if (action.type === "SAVE_COMPREHENSION") {
+      return action.payload;
+    }
+    return state;
+  };
+
+  // support 
+const support = (state = [], action) => {
+    if (action.type === "SAVE_SUPPORT") {
+      return action.payload;
+    }
+    return state;
+  };
+
+    // comments 
+const comment = (state = [], action) => {
+    if (action.type === "SAVE_COMMENT") {
+      return action.payload;
+    }
+    return state;
+  };
+
+// store
+const store = createStore(
+  combineReducers({
+    feeling,
+    comprehension,
+    support,
+    comment,
+    feedback
+  }),
+  applyMiddleware(logger)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
