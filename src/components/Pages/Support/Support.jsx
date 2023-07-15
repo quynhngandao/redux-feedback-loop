e03e0b46a3ab5765988c410cbb37658cab4dca4d
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-15 01:27:55 -0500

    support set up

--- src/components/Pages/Support/Support.jsx
+++ src/components/Pages/Support/Support.jsx
@@ -6,13 +6,15 @@ import { Button } from '@mui/material';
 const ariaLabel = { 'aria-label': 'description' };
 
 function Support() {
+  const [support, setSupport] = useState('')
+  const handleChange = (e) => {
+    setSupport(e.target.support)
+  }
   return (
     <Box component="form" sx={{'& > :not(style)': { m: 1 }}} noValidate autoComplete="off">
-    <Input style = {{width: 500}} label="Support" color="secondary" focused="true" placeholder='How Well Are You Feeling Supported?' inputProps={ariaLabel} />
+    <Input style = {{width: 500}} label="Support" color="secondary" focused="true" placeholder='How Well Are You Feeling Supported?' inputProps={ariaLabel} value={support}/>
     <Button variant="outlined"
-  onClick={() => {
-    alert('clicked');
-  }}
+  onClick={handleChange}
 >Next</Button>
 </Box>
   )
